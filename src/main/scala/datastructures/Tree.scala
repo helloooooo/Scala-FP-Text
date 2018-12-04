c7fc6b099c0c9d3b8ab07772f20d0dab70e4eea4
helloooooo <yomicerisier@gmail.com>
2018-12-04 23:04:33 +0900

    finish Option

--- src/main/scala/datastructures/Tree.scala
+++ src/main/scala/datastructures/Tree.scala
@@ -1,5 +1,6 @@
 package datastructures.Tree
 
+
 sealed trait Tree[+A]
 case class Leaf[A](value:A) extends Tree[A]
 case class Branch[A](left: Tree[A],right:Tree[A]) extends Tree[A]
--- src/main/scala/errorhandling/Option.scala
+++ src/main/scala/errorhandling/Option.scala
@@ -1,8 +1,14 @@
+
+
 sealed trait Option[+A]{
   def map[B](f:A => B):Option[B] = this match {
     case None => None
     case Some(v) => Some(f(v))
   }
+
+  def map2[A,B,C](a:Option[A],b:Option[B])(f:(A,B) => C):Option[C] =
+    a flatMap (aa =>  b map (bb => f(aa,bb)))
+
   def flatMap[B](f:A => Option[B]):Option[B] =
     map(f) getOrElse None
   def getOrElse[B >:A](default: =>B):B = this match {
@@ -10,15 +16,46 @@ sealed trait Option[+A]{
     case Some(v) => v
   }
   def orElse[B >: A](ob: => Option[B]):Option[B] =
-  def filter(f: A => Boolean):Option[A]
+    this map (Some(_)) getOrElse ob
+  def filter(f: A => Boolean):Option[A] =
+    flatMap(a => if (f(a)) Some(a) else None)
+
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
+    a match {
+      case Nil => Some(Nil)
+      case h::t => map2(f(h), traverse(t)(f))(_ :: _)
+    }
 }
 case class Some[+A](get:A) extends Option[A]
 case object None extends Option[Nothing]
 
 
 
-object sub extends Option {
+object Option {
   def mean(xs:Seq[Double]):Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
+
+  def variance(xs:Seq[Double]):Option[Double] =
+    mean(xs) flatMap(m => mean(xs.map(x => math.pow(x - m ,2))))
+
+  def lift[A,B](f:A => B):Option[A] => Option[B] = _ map f
+
+  def parseInsuranceRateQuote(age:String,numberOfSpeedingTickets:String):Option[Double] = {
+    val optAge:Option[Int] = Try(age.toInt)
+    val optTickets:Option[Int] = Try(numberOfSpeedingTickets.toInt)
+
+  }
+
+  def sequence[A](a:List[Option[A]]):Option[List[A]] = a match {
+    case Nil => Some(Nil)
+    case h :: t => h flatMap (hh => sequence(t) map (hh ::_ ))
+  }
+
+
+
+  def Try[A](a: => A):Option[A] =
+    try Some(a)
+    catch {case e:Exception => None}
+  val absO:Option[Double] => Option[Double] = lift(math.abs)
 }
