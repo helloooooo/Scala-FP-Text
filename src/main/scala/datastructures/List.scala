f59a49b2bcc8835c2c9b4236d8d0d7293f756717
helloooooo <yomicerisier@gmail.com>
2020-01-27 22:25:05 +0900

    finish chapter 3

--- src/main/scala/datastructures/List.scala
+++ src/main/scala/datastructures/List.scala
@@ -101,4 +101,52 @@ object List {
 
   def appendOfFoldLeft[A](l: List[A], r:List[A]):List[A] =
     foldRight(l, r)(Cons(_,_))
+
+  def concat[A](l: List[List[A]]): List[A] =
+    foldRight(l, Nil:List[A])(append)
+
+  def add1(l:List[Int]): List[Int] =
+    foldRight(l, Nil:List[Int])((h ,t) => Cons(h+1, t))
+
+  def doubletoString(l:List[Double]):List[String] =
+    foldRight(l, Nil:List[String])((h,t) => Cons(h.toString, t))
+
+  def map[A,B](l:List[A])(f: A => B): List[B] =
+    foldRight(l, Nil:List[B])((h, t) => Cons(f(h), t))
+
+  def filter[A](as: List[A])(f: A => Boolean):List[A] =
+    foldRight(as, Nil:List[A])((h,t) => if (f(h)) Cons(h,t) else t)
+
+  def flatmap[A, B](as:List[A])(f: A => List[B]):List[B] =
+    concat(map(as)(f))
+
+  def filter1[A](l: List[A])(f: A => Boolean):List[A] =
+    flatmap(l)(a => if (f(a)) List(a) else Nil)
+
+  def addDualList(a: List[Int], b:List[Int]):List[Int] = (a,b) match {
+    case (Nil,_) => Nil
+    case (_, Nil) => Nil
+    case (Cons(h1, t1), Cons(h2, t2)) => Cons((h1+h2),addDualList(t1,t2))
+  }
+
+  def zipWith[A,B,C](a:List[A], b:List[B])(f: (A,B) => C):List[C] =
+    (a,b) match {
+      case (Nil,_) => Nil
+      case (_, Nil) => Nil
+      case (Cons(h1, t1), Cons(h2, t2)) => Cons(f(h1,h2), zipWith(t1,t2)(f))
+    }
+
+  @annotation.tailrec
+  def startsWith[A](l: List[A], prefix: List[A]): Boolean = (l,prefix) match {
+    case (_,Nil) => true
+    case (Cons(h,t),Cons(h2,t2)) if h == h2 => startsWith(t, t2)
+    case _ => false
+  }
+
+  @annotation.tailrec
+  def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = sup match {
+    case Nil => sub == Nil
+    case _ if startsWith(sup, sub) => true
+    case Cons(h,t) => hasSubsequence(t, sub)
+  }
 }
